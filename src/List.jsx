054e743e3bc8fa484f58a3518751364b9ecabdbf
ASHISH PRAKASH <60266415+Ashish-Prakash@users.noreply.github.com>
2022-03-06 21:28:25 +0530

    props

---
+++ src/List.jsx
@@ -0,0 +1,24 @@
+let List = (props) =>{
+    return (
+        <ul>
+            {
+                props.input.map((element) =>{
+                    return <li>{element}
+                    {/* <button onClick={
+                        () => {
+                            let currentinput = this.state.input;
+                            let filterTodo = currentinput.filter((e) =>{
+                                return e !== element;
+                            })
+                            this.setState({input : filterTodo})
+                        }
+                    }> Delete</button> */}
+                    </li>
+                })
+            }
+        </ul>
+    )
+}
+export default List;
+
+// https://github.com/Ashish-Prakash/React-Practice.git
